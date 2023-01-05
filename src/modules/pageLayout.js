a123d6fadbd782cfb9fba7e4b0dfdae7c2bdaf40
GutemaG <bir13gu17@gmail.com>
2023-01-05 23:27:08 +0300

    Move some content from pageLayout to index.html

--- src/modules/pageLayout.js
+++ src/modules/pageLayout.js
@@ -1,27 +1,10 @@
-import Logo from '../../assets/images/logo.png';
-import microverseLogo from '../../murple_logo.png';
 import contactUs from './contactUs.js';
 
 const app = document.querySelector('#app');
 const pageLayout = () => {
   app.innerHTML = `
-    <header>
-      <div class="logo">
-        <img src=${Logo} alt="logo image" />
-      </div>
-      <nav class="navbar">
-        <a class="nav-item" href="#" id="meals">Meals(<span class="homeCount"></span>)</a>
-        <a class="nav-item" href="#" id="contact-us">Contact us</a>
-      </nav>
-    </header>
-
     <div class="cardContainer"></div>
     <div class="contact-us"></div>
-    
-    <footer class="footer">
-      <img src=${microverseLogo} alt="microverse logo" />
-      <p>Created by Microverse student under CC Licenses</p>
-    </footer>
   `;
   contactUs();
   document.querySelector('.contact-us').style.display = 'none';
